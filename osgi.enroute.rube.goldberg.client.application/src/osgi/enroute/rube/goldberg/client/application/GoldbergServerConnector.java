c0bec5a85f8e20d0b8c8fe6691c23e863909fa90
Tim Verbelen <tim.verbelen@intec.ugent.be>
2015-04-08 13:09:46 +0200

    keep on trying to connect to server

--- osgi.enroute.rube.goldberg.client.application/src/osgi/enroute/rube/goldberg/client/application/GoldbergServerConnector.java
+++ osgi.enroute.rube.goldberg.client.application/src/osgi/enroute/rube/goldberg/client/application/GoldbergServerConnector.java
@@ -28,6 +28,9 @@ public class GoldbergServerConnector {
 	
 	private String uri = "r-osgi://127.0.0.1:9278";
 	
+	private Thread connectorThread;
+	private volatile boolean running = false;
+	
 	// TODO check for r-osgi exported config? kinda hard coded for now
 	@Reference
 	public void setRemoteServiceAdmin(RemoteServiceAdmin rsa){
@@ -39,35 +42,64 @@ public class GoldbergServerConnector {
 		uri = config.get("uri");
 		// TODO check uri for correctness
 		
-		Map<String, Object> serverEndpointProps = new HashMap<String, Object>();
-		serverEndpointProps.put("endpoint.id", uri);
-		serverEndpointProps.put("service.imported.configs", "be.iminds.aiolos.r-osgi");
-		serverEndpointProps.put("objectClass", new String[]{GoldbergServer.class.getName()});
-		EndpointDescription serverEndpoint = new EndpointDescription(serverEndpointProps);
+		running = true;
+		connectorThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while(running){
+					if(serverImport==null || serverImport.getImportReference()==null){
+						Map<String, Object> serverEndpointProps = new HashMap<String, Object>();
+						serverEndpointProps.put("endpoint.id", uri);
+						serverEndpointProps.put("service.imported.configs", "be.iminds.aiolos.r-osgi");
+						serverEndpointProps.put("objectClass", new String[]{GoldbergServer.class.getName()});
+						EndpointDescription serverEndpoint = new EndpointDescription(serverEndpointProps);
+						
+						serverImport = rsa.importService(serverEndpoint);
+						
+						if(serverImport.getException()!=null){
+							serverImport.close();
+							serverImport = null;
+						}
+						
+						// already import camera listener as well?
+						Map<String, Object> listenerEndpointProps = new HashMap<String, Object>();
+						listenerEndpointProps.put("endpoint.id", uri);
+						listenerEndpointProps.put("service.imported.configs", "be.iminds.aiolos.r-osgi");
+						listenerEndpointProps.put("objectClass", new String[]{CameraListener.class.getName()});
+						EndpointDescription listenerEndpoint = new EndpointDescription(listenerEndpointProps);
+						
+						listenerImport = rsa.importService(listenerEndpoint);
+						
+						if(listenerImport.getException()!=null){
+							listenerImport.close();
+							listenerImport = null;
+						}
+					} 
+					
+					try {
+						// check once every minute...
+						Thread.sleep(60000);
+					} catch (InterruptedException e) {
+					}
+				}
+				
+				if(serverImport!=null){
+					serverImport.close();
+					listenerImport.close();
+				}
+			}
+		});
+		connectorThread.start();
 		
-		serverImport = rsa.importService(serverEndpoint);
-		
-		if(serverImport.getException()!=null){
-			System.err.println("Error connecting to Goldberg server: "+serverImport.getException().getMessage());
-		}
-		
-		// already import camera listener as well?
-		Map<String, Object> listenerEndpointProps = new HashMap<String, Object>();
-		listenerEndpointProps.put("endpoint.id", uri);
-		listenerEndpointProps.put("service.imported.configs", "be.iminds.aiolos.r-osgi");
-		listenerEndpointProps.put("objectClass", new String[]{CameraListener.class.getName()});
-		EndpointDescription listenerEndpoint = new EndpointDescription(listenerEndpointProps);
-		
-		listenerImport = rsa.importService(listenerEndpoint);
-		
-		if(listenerImport.getException()!=null){
-			System.err.println("Error connecting to Goldberg server Camera listener: "+listenerImport.getException().getMessage());
-		}
 	}
 	
 	@Deactivate
 	public void deactivate(){
-		serverImport.close();
-		listenerImport.close();
+		running = false;
+		connectorThread.interrupt();
+		try {
+			connectorThread.join();
+		} catch (InterruptedException e) {
+		}
 	}
 }
